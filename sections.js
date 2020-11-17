d8d40560cfd2f14d583965f662d569ece59000af
Alexander Phan <alexander.phan@me.com>
2020-11-17 02:29:50 -0800

    added two sections bar and area

--- sections.js
+++ sections.js
@@ -1,13 +1,27 @@
-let dataset, svg
+let dataset, svg, genreData
 let salarySizeScale, salaryXScale, categoryColorScale
 let simulation, nodes
 let categoryLegend, salaryLegend
+let currentStep = 0
 
-// const categories = ['Engineering', 'Business', 'Physical Sciences', 'Law & Public Policy', 'Computers & Mathematics', 'Agriculture & Natural Resources',
-// 'Industrial Arts & Consumer Services','Arts', 'Health','Social Science', 'Biology & Life Science','Education','Humanities & Liberal Arts',
-// 'Psychology & Social Work','Communications & Journalism','Interdisciplinary']
 const categories = ['Musical', 'Adventure', 'Drama', 'Comedy', 'Action', 'Horror',
 'Romantic Comedy','Thriller/Suspense', 'Western','Black Comedy', 'Concert/Performance','Documentary', 'Missing']
+
+const genreMap = {
+    "Musical" : 0,
+    "Adventure" : 1,
+    "Drama" : 2,
+    "Comedy" : 3,
+    "Action": 4, 
+    "Horror": 5,
+    "Romantic Comedy" : 6, 
+    "Thriller/Suspense" : 7,
+    "Western" : 8, 
+    "Black Comedy": 9, 
+    "Concert/Performance" : 10, 
+    "Documentary" : 11,
+    "Missing" : 12
+}
 
 const categoriesXY = {'Musical': [0, 550, 57382, 23.9],
                         'Adventure': [600, 800, 43538, 48.3],
@@ -55,6 +69,7 @@ d3.csv('data/disney_movies_total_gross.csv', function(d){
     return {
         title: d["movie_title"],
         releaseDate: d["release_date"],
+        date: d3.timeParse("%b %d, %Y")(d["release_date"]),
         genre: d.genre,
         rating: d["MPAA_rating"],
         totalGross: Number(d["total_gross"].replace(/[^0-9\.-]+/g,"")),
@@ -72,12 +87,56 @@ d3.csv('data/disney_movies_total_gross.csv', function(d){
         return d.genre === "" ? {...d, genre: "Missing"}: d;
     })
 
+    genreData = [
+        {genre: 'Musical',
+        income: 0 }, 
+        {genre: 'Adventure',
+        income: 0 }, 
+        {genre: 'Drama',
+        income: 0 }, 
+        {genre: 'Comedy',
+        income: 0 }, 
+        {genre: 'Action',
+        income: 0 }, 
+        {genre: 'Horror',
+        income: 0 }, 
+        {genre: 'Romantic Comedy',
+        income: 0 }, 
+        {genre: 'Thriller/Suspense',
+        income: 0 }, 
+        {genre: 'Western',
+        income: 0 }, 
+        {genre: 'Black Comedy',
+        income: 0 }, 
+        {genre: 'Concert/Performance',
+        income: 0 }, 
+        {genre: 'Documentary',
+        income: 0 }, 
+        {genre: 'Missing',
+        income: 0 }, 
+    ]
+
+    // 'Adventure', 'Drama', 'Comedy', 'Action', 'Horror',
+    //     'Romantic Comedy','Thriller/Suspense', 'Western','Black Comedy', 'Concert/Performance','Documentary', 'Missing'
+    dataset.forEach(function (d) {
+        let index = genreMap[d.genre ?? "Missing"];
+        // console.log("INDEX", index);
+        genreData[index].income += d.totalGross;
+
+
+    })
+
+    genreData.sort((a,b) => (a.income > b.income) ? -1 : 1)
+    console.log("genre data", genreData);
+
     // dataset = data;
     createScales()
     setTimeout(drawInitial(), 2000)
 })
 
-const colors = ['#ffcc00', '#ff6666', '#cc0066', '#66cccc', '#f688bb', '#65587f', '#baf1a1', '#333333', '#75b79e',  '#66cccc', '#9de3d0', '#f1935c', '#0c7b93', '#eab0d9', '#baf1a1', '#9399ff']
+const colors = ['#ffcc00', '#ff6666', '#cc0066', '#66cccc', '#f688bb', '#65587f', '#baf1a1', '#333333', '#75b79e',  '#66cccc', '#9de3d0', '#f1935c', '#0c7b93']
+
+// , '#eab0d9', '#baf1a1', '#9399ff'
 
 //Create all the scales and save to global variables
 
@@ -85,9 +144,24 @@ function createScales(){
     // salarySizeScale = d3.scaleLinear(d3.extent(dataset, d => d.Median), [5, 35])
     grossIncSizeScale = d3.scaleLinear(d3.extent(dataset, d => d.totalGross), [5, 35])
     // salaryXScale = d3.scaleLinear(d3.extent(dataset, d => d.Median), [margin.left, margin.left + width+250])
-    grossIncXScale = d3.scaleLinear(d3.extent(dataset, d => d.totalGross), [margin.left, margin.left + width+250])
+    grossIncXScale = d3.scaleLinear(d3.extent(dataset, d => d.totalGross), [margin.left, margin.left + width+ 550])
     // salaryYScale = d3.scaleLinear([20000, 110000], [margin.top + height, margin.top])
     categoryColorScale = d3.scaleOrdinal(categories, colors)
+
+    // grossIncYScale = d3.scaleLinear(d3.extent(dataset, d=> d.totalGross), [height / 2, margin.top + 50])
+    grossIncYScale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) { return d.totalGross})]).range([margin.top + height, margin.top])
+    // grossIncYScale = d3.scaleLinear().domain(d3.extent(dataset, d => d.totalGross)).range([height, 0]);
+    timeXScale = d3.scaleTime().domain(d3.extent(dataset, d => d.date)).range([margin.left, margin.left + width])
+    // timeXScale = d3.scaleTime().domain(d3.extent(dataset, d => d.date)).range([0, width])
+
+    // xGenreScale = d3.scaleLinear().domain(d3.extent(genreData, d => d.income)).range([margin.left, margin.left + width])
+    // yGenreScale = d3.scaleBand().range([margin.top, margin.top + height]).domain(genreData.map(d=> d.genre)).padding(.1);
+
+
+    // var areaX = d3.scaleTime().domain(d3.extent(dataset, function(d) {
+    //     return d['release_date']
+    // })).range([0, width]);
+
     // categoryColorScale = d3.scaleOrdinal(d3.schemeCategory20);
     // shareWomenXScale = d3.scaleLinear(d3.extent(dataset, d => d.ShareWomen), [margin.left, margin.left + width])
     // enrollmentScale = d3.scaleLinear(d3.extent(dataset, d => d.Total), [margin.left + 120, margin.left + width - 50])
@@ -179,6 +253,59 @@ function drawInitial(){
             .attr('stroke-opacity', 0.2)
             .attr('stroke-dasharray', 2.5)
 
+
+    
+    let xTimeAxis = d3.axisBottom(timeXScale);
+
+    let xTimeAxisGroup = svg.append('g')
+        .call(xTimeAxis)
+        .attr('class', 'area-x')
+        .attr('opacity', 0)
+        .attr('transform', `translate(0, ${margin.top + height})`)
+        .call(g => g.select('.domain')
+            .remove())
+        .call(g => g.selectAll('.tick line'))
+            .attr('stroke-opacity', 1)
+            .attr('stroke-dasharray', 2.5)
+
+    //             // let scatteryAxis = d3.axisLeft(salaryYScale).tickSize([width])
+
+
+    let yIncAxis = d3.axisLeft(grossIncYScale).tickSize([width]).tickFormat(function(d){ return '$' + d3.format(',')(d)})
+            
+    let yIncAxisGroup = svg.append('g')
+        .call(yIncAxis)
+        .attr('class', 'area-y')
+        .attr('opacity', 0)
+        .attr('transform', `translate(${margin.left + width}, 0)`)
+        .call(g => g.select('.domain')
+            .remove())
+        .call(g => g.selectAll('.tick line'))
+            .attr('stroke-opacity', 0.2)
+            .attr('stroke-dasharray', 2.5)
+    
+    // const lineGenerator = d3.line()
+    //                         .x(d => timeXScale(d.date))    
+    //                         .y(d => grossIncYScale(d.totalGross))    
+
+    // svg.append('g').append("path")
+    //     .attr('class', 'line-path')
+    //     .attr('d', lineGenerator(dataset))
+    // var area = d3.area()
+    //                 .x(function(d) { return timeXScale(d.date)})       
+    //                 .y0(margin.top + height)
+    //                 .y1(function(d) { return grossIncYScale(d.totalGross)})
+
+    // svg.append("path")
+    //     .datum(dataset)
+    //     .attr("fill", "#cce5df")
+    //     .attr("stroke", "#69b3a2")
+    //     .attr("stroke-width", 1.5)
+    //     .attr("d", area
+    //     )
+
+
+
     // Instantiates the force simulation
     // Has no forces. Actual forces are added and removed as required
 
@@ -194,13 +321,15 @@ function drawInitial(){
     // Stop the simulation until later
     simulation.stop()
 
+    // CREATION OF NODES THAT MAKES THE FIRST GRAPH
+
     // Selection of all the circles 
     nodes = svg
         .selectAll('circle')
         .data(dataset)
         .enter()
         .append('circle')
-            .attr('fill', 'black')
+            .attr('fill', '#98a398')
             .attr('r', 3)
             .attr('cx', (d, i) => grossIncXScale(d.totalGross) + 5)
             .attr('cy', (d, i) => i * 5.2 + 30)
@@ -213,8 +342,11 @@ function drawInitial(){
         .on('mouseout', mouseOut)
 
     function mouseOver(d, i){
+        // console.log("curr step", currentStep)
+        if(currentStep == 3 || currentStep == 4) return;
 
-        console.log('hi')
+
+        // console.log('hi')
         d3.select(this)
             .transition('mouseover').duration(100)
             .attr('opacity', 1)
@@ -234,6 +366,9 @@ function drawInitial(){
     }
     
     function mouseOut(d, i){
+        if(currentStep == 3 || currentStep == 4) return;
+
+        // console.log("current setp out", currentStep)
         d3.select('#tooltip')
             .style('display', 'none')
 
@@ -325,6 +460,7 @@ function drawInitial(){
     //     .call(g => g.select('.domain')
     //         .remove())
     
+    
     // svg.append('g')
     //     .call(scatteryAxis)
     //     .attr('class', 'scatter-y')
@@ -345,6 +481,49 @@ function drawInitial(){
     //     .attr('transform', 'translate(0, 700)')
     //     .attr('opacity', 0)
     //     .call(histxAxis)
+
+    // ============================= VISUALIZATION 4 : BAR CHARTS ============================= //
+
+    xGenreScale = d3.scaleLinear().domain(d3.extent(genreData, d => d.income)).range([margin.left, margin.left + width])
+    yGenreScale = d3.scaleBand().range([margin.top, margin.top + height]).domain(genreData.map(d=> d.genre)).padding(.1);
+
+    let xGenreAxis = d3.axisBottom(xGenreScale);
+
+    let xGenreAxisG = svg.append('g')
+        .attr('class', 'genre-chart-x')
+        .attr('transform', `translate(0, ${height + margin.top})`)
+        .attr('opacity', 0)
+        .call(xGenreAxis)
+        .call(g => g.select('.domain').remove())
+    
+    let yGenreAxis = d3.axisLeft(yGenreScale)
+
+    let yGenreAxisG = svg.append('g')    
+        .attr('class', 'genre-chart-y')
+        .attr('opacity', 0)
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(yGenreAxis)
+
+
+    svg.selectAll('myRect')
+        .data(genreData)
+        .enter()
+        .append("rect")
+        .attr('class', "genre-rects")
+        .attr('opacity', 0)
+        .attr("x", xGenreScale(0))
+        .attr("y", function(d){
+            console.log("doing", d.genre)
+            return yGenreScale(d.genre);
+        })
+        .attr("width", function(d){
+            // console.log("wow", yGenreScale(100000));
+            return xGenreScale(0)
+        })
+        .attr("height", yGenreScale.bandwidth())
+        .attr("fill", function(d){
+            return colors[genreMap[d.genre]];
+        })
 }
 
 //Cleaning Function
@@ -374,11 +553,24 @@ function clean(chartType){
     if (chartType !== "isBubble"){
         svg.select('.enrolment-axis').transition().attr('opacity', 0)
     }
+    if (chartType !== "isArea"){
+        svg.select('.area-x').transition().attr('opacity', 0)
+        svg.select('.area-y').transition().attr('opacity', 0)
+    }
+    if(chartType !== "isBarChart"){
+        console.log("Is this cleaning")
+        svg.select('.genre-chart-x').transition().attr('opacity', 0)
+        svg.select('.genre-chart-y').transition().attr('opacity', 0)
+        svg.selectAll('.genre-rects').transition().attr('opacity', 0).attr("width", function(d){
+            return xGenreScale(0)
+        })
+    }
 }
 
 //First draw function
 
 function draw1(){
+    currentStep = 1
     //Stop simulation
     simulation.stop()
     
@@ -396,7 +588,7 @@ function draw1(){
     
     svg.selectAll('circle')
         .transition().duration(500).delay(100)
-        .attr('fill', 'black')
+        .attr('fill', '#98a398')
         .attr('r', 3)
         .attr('cx', (d, i) => grossIncXScale(d.totalGross)+5)
         .attr('cy', (d, i) => i * 5.2 + 30)
@@ -409,14 +601,17 @@ function draw1(){
 
 
 function draw2(){
+    currentStep = 2;
     let svg = d3.select("#vis").select('svg')
+
     
     clean('none')
 
     svg.selectAll('circle')
-        .transition().duration(300).delay((d, i) => i * 5)
+        .transition().duration(300).delay((d, i) => i * 1)
         .attr('r', d => grossIncSizeScale(d.totalGross) * 1.2)
         .attr('fill', d => categoryColorScale(d.genre))
+        .attr('opacity', 1)
 
     simulation  
         .force('charge', d3.forceManyBody().strength([2]))
@@ -435,41 +630,140 @@ function draw2(){
 }
 
 function draw3(){
+    currentStep = 3;
+    console.log("DRWING 3");
     let svg = d3.select("#vis").select('svg')
-    clean('isMultiples')
+    clean('isArea')
+
+    svg.select('genre-chart-x').transition().attr('opacity', 0)
+        svg.select('genre-chart-y').transition().attr('opacity', 0)
+        svg.selectAll('genre-rects').transition().attr('opacity', 0)
     
     svg.selectAll('circle')
-        .transition().duration(400).delay((d, i) => i * 5)
-        .attr('r', d => salarySizeScale(d.Median) * 1.2)
-        .attr('fill', d => categoryColorScale(d.Category))
+        .transition().duration(100).delay((d, i) => i * 2)
+        .attr("opacity", 0)
+        .attr('cx', margin.left + width)
+        .attr('cy', height /2)
 
-    svg.selectAll('.cat-rect').transition().duration(300).delay((d, i) => i * 30)
-        .attr('opacity', 0.2)
-        .attr('x', d => categoriesXY[d][0] + 120)
+    svg.select('.area-x').transition().duration(1400).attr('opacity', 1);
+    svg.select('.area-y').transition().duration(1400).attr('opacity', 1);
+
+
+    // var areaX = d3.scaleTime().domain(d3.extent(dataset, function(d) {
+    //     return d['release_date']
+    // })).range([0, width]);
+
+    // svg.append("g").attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(x));
+
+
+
         
-    svg.selectAll('.lab-text').transition().duration(300).delay((d, i) => i * 30)
-        .text(d => `Average: $${d3.format(",.2r")(categoriesXY[d][2])}`)
-        .attr('x', d => categoriesXY[d][0] + 200)   
-        .attr('y', d => categoriesXY[d][1] + 50)
+        // .attr('r', d => salarySizeScale(d.Median) * 1.2)
+        // .attr('fill', d => categoryColorScale(d.Category))
+
+    // svg.selectAll('.cat-rect').transition().duration(300).delay((d, i) => i * 30)
+    //     .attr('opacity', 0.2)
+    //     .attr('x', d => categoriesXY[d][0] + 120)
+        
+    // svg.selectAll('.lab-text').transition().duration(300).delay((d, i) => i * 30)
+    //     .text(d => `Average: $${d3.format(",.2r")(categoriesXY[d][2])}`)
+    //     .attr('x', d => categoriesXY[d][0] + 200)   
+    //     .attr('y', d => categoriesXY[d][1] + 50)
+    //     .attr('opacity', 1)
+
+    // svg.selectAll('.lab-text')
+    //     .on('mouseover', function(d, i){
+    //         d3.select(this)
+    //             .text(d)
+    //     })
+    //     .on('mouseout', function(d, i){
+    //         d3.select(this)
+    //             .text(d => `Average: $${d3.format(",.2r")(categoriesXY[d][2])}`)
+    //     })
+
+    // simulation  
+    //     .force('charge', d3.forceManyBody().strength([2]))
+    //     .force('forceX', d3.forceX(d => categoriesXY[d.Category][0] + 200))
+    //     .force('forceY', d3.forceY(d => categoriesXY[d.Category][1] - 50))
+    //     .force('collide', d3.forceCollide(d => salarySizeScale(d.Median) + 4))
+    //     .alpha(0.7).alphaDecay(0.02).restart()
+
+}
+
+function draw4(){
+
+    // currentStep = 4;
+    // console.log("DRWING 4");
+    // let svg = d3.select("#vis").select('svg')
+    // clean('isBarChart')
+    
+    // svg.selectAll('circle')
+    //     .transition().duration(100).delay((d, i) => i * 2)
+    //     .attr("opacity", 0)
+    //     .attr('cx', margin.left + width)
+    //     .attr('cy', height /2)
+
+    // svg.select('.area-x').transition().duration(1400).attr('opacity', 1);
+    // svg.select('.area-y').transition().duration(1400).attr('opacity', 1);
+
+
+    currentStep = 4;
+    clean('isBarChart')
+    console.log("drawing 4");
+    let svg = d3.select('#vis').select('svg')
+
+    svg.select('.genre-chart-x').transition().duration(1400).attr('opacity', 1);
+    svg.select('.genre-chart-y').transition().duration(1400).attr('opacity', 1);
+    svg.selectAll('.genre-rects')
+        .transition()
+        .duration(1400)
+        .attr("width", d => xGenreScale(d.income))
+        .attr('x', d=> xGenreScale(0) )
         .attr('opacity', 1)
+        .delay(function(d,i) {
+        return(i * 100)
+    })
+    // svg.selectAll('circle')
+    //     .transition().duration(100).delay((d, i) => i * 5)
+    //     .attr("opacity", 1);
 
-    svg.selectAll('.lab-text')
-        .on('mouseover', function(d, i){
-            d3.select(this)
-                .text(d)
-        })
-        .on('mouseout', function(d, i){
-            d3.select(this)
-                .text(d => `Average: $${d3.format(",.2r")(categoriesXY[d][2])}`)
-        })
+    //     svg.selectAll('circle')
+    //     .transition().duration(300).delay((d, i) => i * 3)
+    //     .attr('r', d => grossIncSizeScale(d.totalGross) * 1.2)
+    //     .attr('fill', d => categoryColorScale(d.genre))
+    //     .attr('opacity', 1)
 
-    simulation  
-        .force('charge', d3.forceManyBody().strength([2]))
-        .force('forceX', d3.forceX(d => categoriesXY[d.Category][0] + 200))
-        .force('forceY', d3.forceY(d => categoriesXY[d.Category][1] - 50))
-        .force('collide', d3.forceCollide(d => salarySizeScale(d.Median) + 4))
-        .alpha(0.7).alphaDecay(0.02).restart()
+    // simulation  
+    //     .force('charge', d3.forceManyBody().strength([2]))
+    //     .force('forceX', d3.forceX(function(d) {
+    //         console.log("CHANGIN")
+    //         console.log(d.genre);
+    //         return categoriesXY[d.genre][0] + 200
+    //     } ))
+    //     .force('forceY', d3.forceY(d => categoriesXY[d.genre][1] - 50))
+    //     .force('collide', d3.forceCollide(d => grossIncSizeScale(d.totalGross) + 4))
+    //     .alphaDecay([0.02])
 
+    
+
+    // simulation.stop()
+
+    // svg.selectAll('circle')
+    //     .transition().duration(600).delay((d, i) => i * 2).ease(d3.easeBack)
+    //         .attr('r', 10)
+    //         .attr('cx', d => histXScale(d.Midpoint))
+    //         .attr('cy', d => histYScale(d.HistCol))
+    //         .attr('fill', d => categoryColorScale(d.Category))
+
+    // let xAxis = d3.axisBottom(histXScale)
+    // svg.append('g')
+    //     .attr('class', 'hist-axis')
+    //     .attr('transform', `translate(0, ${height + margin.top + 10})`)
+    //     .call(xAxis)
+
+    // svg.selectAll('.lab-text')
+    //     .on('mouseout', )
 }
 
 function draw5(){
@@ -565,29 +859,6 @@ function draw7(){
 
 }
 
-function draw4(){
-    let svg = d3.select('#vis').select('svg')
-
-    clean('isHist')
-
-    simulation.stop()
-
-    svg.selectAll('circle')
-        .transition().duration(600).delay((d, i) => i * 2).ease(d3.easeBack)
-            .attr('r', 10)
-            .attr('cx', d => histXScale(d.Midpoint))
-            .attr('cy', d => histYScale(d.HistCol))
-            .attr('fill', d => categoryColorScale(d.Category))
-
-    let xAxis = d3.axisBottom(histXScale)
-    svg.append('g')
-        .attr('class', 'hist-axis')
-        .attr('transform', `translate(0, ${height + margin.top + 10})`)
-        .call(xAxis)
-
-    svg.selectAll('.lab-text')
-        .on('mouseout', )
-}
 
 function draw8(){
     clean('none')
